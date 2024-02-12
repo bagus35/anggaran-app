23e24b92788887ff0e94bec8d791495de9d72512
Jauhara Nada <nadajauhara90@gmail.com>
2024-02-12 19:17:34 +0700

    gambar header

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -15,7 +15,7 @@
                                 <div class="row align-items-center">
                                     <div class="col-8">
                                         <h4 class="text-c-purple">$30200</h4>
-                                        <h6 class="text-muted m-b-0">Jumlah Anggaran 2024</h6>
+                                        <h6 class="text-muted m-b-0">Anggaran Tahun 2024</h6>
                                     </div>
                                     <div class="col-4 text-right">
                                         <i class="fa fa-bar-chart f-28"></i>
@@ -35,7 +35,7 @@
                                 <div class="row align-items-center">
                                     <div class="col-8">
                                         <h4 class="text-c-green">290+</h4>
-                                        <h6 class="text-muted m-b-0">Jumlah Anggaran Terserap</h6>
+                                        <h6 class="text-muted m-b-0">Anggaran Terserap</h6>
                                     </div>
                                     <div class="col-4 text-right">
                                         <i class="fa fa-file-text-o f-28"></i>
@@ -54,7 +54,7 @@
                                 <div class="row align-items-center">
                                     <div class="col-8">
                                         <h4 class="text-c-red">145</h4>
-                                        <h6 class="text-muted m-b-0">Jumlah Aggaran Belum Terserap</h6>
+                                        <h6 class="text-muted m-b-0">Anggaran Belum Terserap</h6>
                                     </div>
                                     <div class="col-4 text-right">
                                         <i class="fa fa-calendar-check-o f-28"></i>
@@ -66,59 +66,73 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    
-                    <!-- task, page, download counter  end -->
-
-                    <div class="card">
-                        <div class="card-header">
-                            <h5>Data Saldo Per Unit</h5>
-                            <div class="card-header-right">
-                                <ul class="list-unstyled card-option">
-                                    <li><i class="fa fa fa-wrench open-card-option"></i></li>
-                                    <li><i class="fa fa-window-maximize full-card"></i></li>
-                                    <li><i class="fa fa-minus minimize-card"></i></li>
-                                    <li><i class="fa fa-refresh reload-card"></i></li>
-                                    <li><i class="fa fa-trash close-card"></i></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="card-block table-border-style">
-                            <div class="table-responsive">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>First Name</th>
-                                            <th>Last Name</th>
-                                            <th>Username</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">1</th>
-                                            <td>Mark</td>
-                                            <td>Otto</td>
-                                            <td>@mdo</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">2</th>
-                                            <td>Jacob</td>
-                                            <td>Thornton</td>
-                                            <td>@fat</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">3</th>
-                                            <td>Larry</td>
-                                            <td>the Bird</td>
-                                            <td>@twitter</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-
+                    </div>                                 
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h5>Data Saldo Per Unit</h5>
+                <div class="card-header-right">
+                    <ul class="list-unstyled card-option">
+                        <li><i class="fa fa fa-wrench open-card-option"></i></li>
+                        <li><i class="fa fa-window-maximize full-card"></i></li>
+                        <li><i class="fa fa-minus minimize-card"></i></li>
+                        <li><i class="fa fa-refresh reload-card"></i></li>
+                        <li><i class="fa fa-trash close-card"></i></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="card-block table-border-style">
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Nama Unit</th>
+                                <th>Jumlah Anggaran</th>
+                                <th>Anggaran Terserap</th>
+                                <th>Anggaran Belum Terserap</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Unit A</td>
+                                <td>7.500.000</td>
+                                <td>5.000.000</td>
+                                <td>2.500.000</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Unit B</td>
+                                <td>7.800.000</td>
+                                <td>3.500.000</td>
+                                <td>3.800.000</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Unit C</td>
+                                <td>7.500.000</td>
+                                <td>5.000.000</td>
+                                <td>2.500.000</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>Unit D</td>
+                                <td>5.500.000</td>
+                                <td>5.000.000</td>
+                                <td>500.000</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>Unit E</td>
+                                <td>9.000.000</td>
+                                <td>5.000.000</td>
+                                <td>4.000.000</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
         <div id="styleSelector"> </div>
     </div>
