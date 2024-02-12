ee8484fd431f061d0c4f1422afd0fa858bd1c3c0
Jauhara Nada <nadajauhara90@gmail.com>
2024-02-12 19:26:13 +0700

    icon link

--- resources/views/user/layouts/app.blade.php
+++ resources/views/user/layouts/app.blade.php
@@ -7,7 +7,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <!-- Favicon icon -->
-      <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
+      <link rel="icon" href="assets/images/logo-poliwangi.ico" type="image/x-icon">
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
     <!-- waves.css -->
